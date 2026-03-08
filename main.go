cf2983e0441691b9efe78cf31480f3ef3af76c0a
alonsoF100 <yanul228@mail.ru>
2026-03-08 16:14:08 +0300

    add data-race to main code second try

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
     var wg sync.WaitGroup
-    wg.Add(5)
+    wg.Add(100)
 
-    for i := 0; i < 5; i++ {
+    for i := 0; i < 100; i++ {
         go func() {
             fmt.Println(i)
             wg.Done()
